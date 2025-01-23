0439457ca7b2ae5347b23372b47c9a26f12c2175
swatirajesh277 <swatirajesh727@gmail.com>
2025-01-23 15:42:33 +0800

    add echo except bye

--- src/main/java/CherryBot.java
+++ src/main/java/CherryBot.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
 public class CherryBot {
     public static void main(String[] args) {
-        System.out.println("________________________________________");
+        System.out.println("\t________________________________________");
         System.out.println("Hello! I'm CherryBot");
         System.out.println("What can I do for you?");
-        System.out.println("________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("________________________________________");
+        System.out.println("\t________________________________________");
+
+        Scanner msg = new Scanner(System.in);
+        String userInput;
+
+        while(true) {
+            userInput = msg.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println("\t________________________________________");
+                System.out.println("\tBye. See you later alligator!");
+                System.out.println("\t________________________________________");
+                break;
+            } else {
+                System.out.println("\t________________________________________");
+                System.out.println("\t" + userInput);
+                System.out.println("\t________________________________________");
+            }
+        }
+        msg.close();
+
     }
 }
