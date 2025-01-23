591bd9dbbaafbe009a3b4363f63aa7c82933de34
swatirajesh277 <swatirajesh727@gmail.com>
2025-01-23 15:33:42 +0800

    replace name and add bot intro

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# CherryBot User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/CherryBot.java
@@ -0,0 +1,10 @@
+public class CherryBot {
+    public static void main(String[] args) {
+        System.out.println("________________________________________");
+        System.out.println("Hello! I'm CherryBot");
+        System.out.println("What can I do for you?");
+        System.out.println("________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________________");
+    }
+}
