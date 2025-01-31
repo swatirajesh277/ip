945fa54212f061bc3999a5be24ba1e2f94d229a8
swatirajesh277 <swatirajesh727@gmail.com>
2025-01-31 14:07:46 +0800

    add ui class

---
+++ src/main/java/Ui.java
@@ -0,0 +1,19 @@
+public class Ui {
+
+    public static void greeting() {
+        System.out.println("\t________________________________________");
+        System.out.println("Hello! I'm CherryBot");
+        System.out.println("What can I do for you?");
+        System.out.println("\t________________________________________");
+    }
+
+    public static void goodbye() {
+        System.out.println("\tBye. See you later alligator!");
+        System.out.println("\t________________________________________");
+    }
+
+    public static void showLine() {
+        System.out.println("\t________________________________________");
+    }
+
+}
